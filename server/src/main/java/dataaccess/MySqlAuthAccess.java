c80d818f83ecd710c6fd88ec7f6b0b799528f560
kalipower <kalijpower@icloud.com>
2025-05-28 17:29:34 -0600

    sql auth access initial

--- server/src/main/java/dataaccess/MySqlAuthAccess.java
+++ server/src/main/java/dataaccess/MySqlAuthAccess.java
@@ -1,32 +1,125 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.AuthData;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.UUID;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 public class MySqlAuthAccess implements AuthAccess {
+    public MySqlAuthAccess() throws DataAccessException, SQLException {
+        configureDatabase();
+    }
 
+    private static final HashMap<String, AuthData> DATA_HASH_MAP = new HashMap<>();
 
-    @Override
     public boolean isEmpty() throws DataAccessException {
-        return false;
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT COUNT(*) FROM auth";
+            try (var ps = conn.prepareStatement(statement);
+                 var rs = ps.executeQuery()) {
+                rs.next();
+                int count = rs.getInt(1);
+                return count == 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to check if auth table is empty");
+        }
     }
 
-    @Override
     public String createAuth(String username) throws DataAccessException {
-        return "";
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO auth (username, authToken) VALUES(?, ?)";
+            try (var ps = conn.prepareStatement(statement)) {
+                String token = UUID.randomUUID().toString();
+                ps.setString(1, username);
+                ps.setString(2, token);
+                ps.executeUpdate();
+                return token;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data");
+        }
     }
 
-    @Override
     public AuthData getAuth(String token) throws DataAccessException {
-        return null;
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username FROM auth WHERE authToken=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, token);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        var username = rs.getString("username");
+                        return new AuthData(username, token);
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to read data");
+        }
     }
 
-    @Override
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof AuthData p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("unable to update database");
+        }
+    }
+
+
     public void clear() throws DataAccessException {
-
+        var statement = "TRUNCATE TABLE auth";
+        executeUpdate(statement);
     }
 
-    @Override
     public void deleteToken(String token) throws DataAccessException {
+        var statement = "DELETE FROM auth WHERE authToken=?";
+        executeUpdate(statement, token);
+    }
 
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  auth (
+              username varchar(256) NOT NULL,
+              authToken varchar(256) NOT NULL,
+              PRIMARY KEY (authToken)
+            )"""
+    };
+
+    private void configureDatabase() throws SQLException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database"));
+        }
     }
 }
